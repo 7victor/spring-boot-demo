f8325429721cd0c01a62b772911933d333eb4132
victor <victor@172.20.65.18>
2018-01-22 17:53:40 +0800

    修改websocket内容

--- src/main/java/com/example/SpringBootDemoApplication.java
+++ src/main/java/com/example/SpringBootDemoApplication.java
@@ -17,35 +17,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.websocket.SocketMessage;
 @SpringBootApplication
-@Controller
-@EnableScheduling
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
-	@Autowired
-    private SimpMessagingTemplate messagingTemplate;
-
-    @GetMapping("/websocket")
-    public String index() {
-        return "index";
-    }
-
-    @MessageMapping("/send")
-    @SendTo("/topic/send")
-    public SocketMessage send(SocketMessage message) throws Exception {
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        message.date = df.format(new Date());
-        return message;
-    }
-
-    @Scheduled(fixedRate = 1000)
-    @SendTo("/topic/callback")
-    public Object callback() throws Exception {
-        // 发现消息
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        messagingTemplate.convertAndSend("/topic/callback", df.format(new Date()));
-        return "callback";
-    }
+	
 }
